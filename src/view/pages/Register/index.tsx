2201467a5e14747514ef84add1be8ba33e263e26
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-13 23:28:12 -0300

    feat: create register form

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,3 +1,46 @@
+import { Link } from 'react-router-dom'
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+
 export function Register() {
-  return <h1>RegisterPage</h1>
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
+        <p className="space-x-2 tracking-[-0.5px]">
+          <span className="text-gray-700 ">Já possui uma conta?</span>
+          <Link to={'/login'} className="font-medium text-teal-900">
+            Fazer Login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          type="text"
+          placeholder="Nome"
+          name="name"
+          autoComplete="full-name"
+        />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+          autoComplete="email"
+        />
+        <Input
+          type="password"
+          placeholder="senha"
+          name="password"
+          autoComplete="current-password"
+        />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
+  )
 }
