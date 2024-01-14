4e8c09fcb5c333783efaefc554ac8ca430bc7472
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-13 23:23:58 -0300

    refactor: toggle div by fragment

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -4,7 +4,7 @@ import { Button } from '../../components/Button'
 
 export function Login() {
   return (
-    <div>
+    <>
       <header className="flex flex-col items-center gap-4">
         <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
           Entre em sua conta
@@ -35,6 +35,6 @@ export function Login() {
           Entrar
         </Button>
       </form>
-    </div>
+    </>
   )
 }
