78425182680c239397120e5978bd712fab064b73
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-13 23:20:20 -0300

    feat: create Login form

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,3 +1,40 @@
+import { Link } from 'react-router-dom'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
 export function Login() {
-  return <h1>LoginPage</h1>
+  return (
+    <div>
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
+        <p className="space-x-2 tracking-[-0.5px]">
+          <span className="text-gray-700 ">Novo por aqui?</span>
+          <Link to={'/register'} className="font-medium text-teal-900">
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+          autoComplete="username"
+        />
+        <Input
+          type="password"
+          placeholder="senha"
+          name="password"
+          autoComplete="current-password"
+        />
+
+        <Button type="submit" className="mt-2">
+          Entrar
+        </Button>
+      </form>
+    </div>
+  )
 }
