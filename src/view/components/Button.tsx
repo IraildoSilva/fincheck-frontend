2f1c4d3312a3225dde05de983b0c72e2ca1195dc
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-14 13:54:20 -0300

    fix: margin top class

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,10 +1,15 @@
+import { cn } from '../../app/utils/cn'
+
 interface ButtonProps extends React.ComponentProps<'button'> {}
 
-export function Button({ ...props }: ButtonProps) {
+export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 transition-all px-6 h-12 rounded-2xl text-white font-medium"
+      className={cn(
+        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 transition-all px-6 h-12 rounded-2xl text-white font-medium',
+        className
+      )}
     />
   )
 }
