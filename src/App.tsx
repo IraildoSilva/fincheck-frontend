29411c04035e3c0d83ee4ff4db2deb891f955e22
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-17 23:52:17 -0300

    feat: install react query developer tool

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Router } from './Router'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './app/contexts/AuthContext'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,6 +23,8 @@ export function App() {
 
         <Toaster />
       </AuthProvider>
+
+      <ReactQueryDevtools position="top" buttonPosition="top-right" />
     </QueryClientProvider>
   )
 }
