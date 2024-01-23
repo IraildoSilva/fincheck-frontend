e676c647c6bee877cd9dfc8c49059ba7695d2437
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-23 19:06:54 -0300

    refactor: change import of SliderNavigation

--- src/view/pages/Dashboard/components/Accounts/index.tsx
+++ src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -3,7 +3,7 @@ import { AccountCard } from './AccountCard'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 import 'swiper/css'
-import { AccountsSliderNavigation } from './AccountsSliderNavigation'
+import { SliderNavigation } from './SliderNavigation'
 import { useAccountsController } from './useAccountsController'
 
 export function Accounts() {
@@ -45,7 +45,7 @@ export function Accounts() {
                 Minhas contas
               </strong>
 
-              <AccountsSliderNavigation
+              <SliderNavigation
                 isBeginning={sliderState.isBeginning}
                 isEnd={sliderState.isEnd}
               />
