70c929637516be2fd8bd11920707480c160bc387
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-23 21:08:03 -0300

    feat: create dashboard context

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -2,25 +2,28 @@ import { UserMenu } from '../../components/UserMenu'
 import { Logo } from '../../components/Logo'
 import { Accounts } from './components/Accounts'
 import { Transactions } from './components/Transactions'
+import { DashboardProvider } from './components/DashboardContext'
 
 export function Dashboard() {
   return (
-    <div className="h-full w-full max-h-screen p-4 lg:px-8 lg:pb-8 lg:pt-6 flex flex-col gap-4">
-      <header className="h-12 flex items-center justify-between">
-        <Logo className="text-teal-900 h-6" />
-        <UserMenu />
-      </header>
+    <DashboardProvider>
+      <div className="h-full w-full max-h-screen p-4 lg:px-8 lg:pb-8 lg:pt-6 flex flex-col gap-4">
+        <header className="h-12 flex items-center justify-between">
+          <Logo className="text-teal-900 h-6" />
+          <UserMenu />
+        </header>
 
-      <main className="flex-1 flex flex-col lg:flex-row gap-4 max-h-full">
-        <div className="w-full h-full lg:w-1/2">
-          {/* className="w-full h-full lg:w-1/2" */}
+        <main className="flex-1 flex flex-col lg:flex-row gap-4 max-h-full">
+          <div className="w-full h-full lg:w-1/2">
+            {/* className="w-full h-full lg:w-1/2" */}
 
-          <Accounts />
-        </div>
-        <div className="w-full h-full lg:w-1/2">
-          <Transactions />
-        </div>
-      </main>
-    </div>
+            <Accounts />
+          </div>
+          <div className="w-full h-full lg:w-1/2">
+            <Transactions />
+          </div>
+        </main>
+      </div>
+    </DashboardProvider>
   )
 }
