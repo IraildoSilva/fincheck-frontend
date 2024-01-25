f346c427c1a9bc29eefcd4fde9033d17b67ac892
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-24 21:25:10 -0300

    feat: implements user drop down menu

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,9 +1,29 @@
+import { ExitIcon } from '@radix-ui/react-icons'
+import { DropdownMenu } from './DropdownMenu'
+import { useAuth } from '../../app/hooks/useAuth'
+
 export function UserMenu() {
+  const { signout } = useAuth()
+
   return (
-    <div className="w-12 h-12 bg-teal-50 rounded-full p-5 flex items-center justify-center border border-teal-100">
-      <span className="text-teal-900 text-sm font-medium tracking-[-0.5px]">
-        IR
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="w-12 h-12 bg-teal-50 rounded-full p-5 flex items-center justify-center border border-teal-100">
+          <span className="text-teal-900 text-sm font-medium tracking-[-0.5px]">
+            IR
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item
+          onSelect={signout}
+          className="flex items-center justify-between"
+        >
+          Sair
+          <ExitIcon className="w-5 h-5" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   )
 }
