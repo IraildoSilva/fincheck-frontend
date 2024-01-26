49539b04e221b2407993edca5946a01be850d4e3
Iraildo Silva <iraildosilva1999@gmail.com>
2024-01-26 20:04:13 -0300

    fixed: change default FiltersModal state open to false

--- src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,31 +1,31 @@
-import { useCallback, useState } from 'react'
-import { useDashboard } from '../DashboardContext/useDashboard'
+import { useCallback, useState } from "react";
+import { useDashboard } from "../DashboardContext/useDashboard";
 
 export function useTransactionsController() {
-  const { areValuesVisible } = useDashboard()
-  const [slideState, setSlideState] = useState({
-    isBeginning: true,
-    isEnd: false,
-  })
-  const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(true)
+	const { areValuesVisible } = useDashboard();
+	const [slideState, setSlideState] = useState({
+		isBeginning: true,
+		isEnd: false,
+	});
+	const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false);
 
-  const handleOpenFiltersModal = useCallback(() => {
-    setIsFiltersModalOpen(true)
-  }, [])
+	const handleOpenFiltersModal = useCallback(() => {
+		setIsFiltersModalOpen(true);
+	}, []);
 
-  const handleCloseFiltersModal = useCallback(() => {
-    setIsFiltersModalOpen(false)
-  }, [])
+	const handleCloseFiltersModal = useCallback(() => {
+		setIsFiltersModalOpen(false);
+	}, []);
 
-  return {
-    slideState,
-    setSlideState,
-    areValuesVisible,
-    isInitialLoading: false,
-    transactions: [''],
-    isLoading: false,
-    isFiltersModalOpen,
-    handleOpenFiltersModal,
-    handleCloseFiltersModal,
-  }
+	return {
+		slideState,
+		setSlideState,
+		areValuesVisible,
+		isInitialLoading: false,
+		transactions: [""],
+		isLoading: false,
+		isFiltersModalOpen,
+		handleOpenFiltersModal,
+		handleCloseFiltersModal,
+	};
 }
