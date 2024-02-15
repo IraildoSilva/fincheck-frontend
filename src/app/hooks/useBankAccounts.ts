2701ecde056a9479e07670b75c32e6e0f743ab8f
Iraildo Silva <iraildosilva1999@gmail.com>
2024-02-15 20:20:37 -0300

    feat: change bank accounts and categories mutation stale time to infinity

--- src/app/hooks/useBankAccounts.ts
+++ src/app/hooks/useBankAccounts.ts
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query'
 import { bankAccountsService } from '../services/bankAccountsService'
 
 export function useBankAccounts() {
-	const { data, isFetching } = useQuery({
-		queryKey: ['bankAccounts'],
-		queryFn: bankAccountsService.getAll,
-	})
+  const { data, isFetching } = useQuery({
+    queryKey: ['bankAccounts'],
+    queryFn: bankAccountsService.getAll,
+    staleTime: Infinity,
+  })
 
-	return { accounts: data ?? [], isFetching }
+  return { accounts: data ?? [], isFetching }
 }
--- src/app/hooks/useCategories.ts
+++ src/app/hooks/useCategories.ts
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query'
 import { categoriesService } from '../services/categoriesService'
 
 export function useCategories() {
-	const { data, isFetching } = useQuery({
-		queryKey: ['categories'],
-		queryFn: categoriesService.getAll,
-	})
+  const { data, isFetching } = useQuery({
+    queryKey: ['categories'],
+    queryFn: categoriesService.getAll,
+    staleTime: Infinity,
+  })
 
-	return { categories: data ?? [], isFetching }
+  return { categories: data ?? [], isFetching }
 }
